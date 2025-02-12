e38e3d0a9c00eb60281da2c179691d48f11e346f
BeluRenda <75823704+BeluRenda@users.noreply.github.com>
2025-02-12 13:03:11 -0300

    initial commit

---
+++ Sistema_SmartHomeIOT/src/negocio/DispositivoABM.java
@@ -0,0 +1,26 @@
+package negocio;
+
+import java.util.List;
+
+import dao.DispositivoDao;
+import datos.Dispositivo;
+
+public class DispositivoABM {
+	DispositivoDao dao = new DispositivoDao();
+	
+//Casos de Uso:
+	
+		public List<Dispositivo> traer(){
+			return dao.traer();
+		}
+		
+
+		public Dispositivo traer(String nombre) {
+			return dao.traer(nombre);
+		}
+		
+
+		public List<Dispositivo> traerTodosLosDispositivosSensorTemperatura() {
+			return dao.traerTodosLosDispositivosSensorTemperatura();
+		}
+}
